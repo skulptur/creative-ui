da2b934f2e63a3d7fad1e1e0cdd8778ca762004d
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-05-26 18:40:55 -0300

    Update index.tsx

--- example/index.tsx
+++ example/index.tsx
@@ -1,1 +1,14 @@
-const example = () => {};
+import 'react-app-polyfill/ie11'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { Loader } from '../src'
+
+const App = () => {
+  return (
+    <div>
+      <Loader></Loader>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
